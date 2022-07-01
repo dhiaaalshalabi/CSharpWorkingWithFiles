752d59dfdc2b4018bc1a23575f13b7d261568306
Dhiaa Shalabi <dhiaa.shalabi@gmail.com>
2022-07-01 07:27:46 +0300

    Add demo file and fileinfo.

--- WorkingWithFiles/Program.cs
+++ WorkingWithFiles/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// The File class
+var path = @"c:\somefile\myfile\myfile.jpg";
+File.Copy(@"c:\temp\myfile\myfile.jpg",@"d:\temp\myfile.jpg",true);
+File.Delete(path);
+if (File.Exists(path))
+{
+    //
+}
 
-Console.WriteLine("Hello, World!");
+var content = File.ReadAllText(path);
+ 
+// The FileInfo class
+ 
+var fileInfo = new FileInfo(path);
+fileInfo.CopyTo("...");
+fileInfo.Delete();
+if (fileInfo.Exists)
+{
+    //
+}
